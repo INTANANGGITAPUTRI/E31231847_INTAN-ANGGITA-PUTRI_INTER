30df9c6f36148eff796db828ce09c1602388ee20
INTANANGGITAPUTRI <intananggitapklh@gmail.com>
2024-01-29 16:38:15 +0700

    Create No2.java

---
+++ No2.java
@@ -0,0 +1,17 @@
+package acara9;
+public class No2 {
+    public static void main(String[] args) {
+ int number = 10;
+ // checks if number is greater than 0
+ if (number > 0) {
+ System.out.println("The number is positive.");
+ }
+
+ // execute this block
+ // if number is not greater than 0
+ else {
+ System.out.println("The number is not positive.");
+ }
+ System.out.println("Statement outside if...else block");
+ }
+}

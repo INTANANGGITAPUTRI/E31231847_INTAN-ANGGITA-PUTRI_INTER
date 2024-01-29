32fab3f43c9c87c1f05f5063fa48ab33f181d1d2
INTANANGGITAPUTRI <intananggitapklh@gmail.com>
2024-01-29 16:32:11 +0700

    Create No1.java

---
+++ No1.java
@@ -0,0 +1,12 @@
+package acara9;
+public class No1 {
+
+    public static void main(String[] args) {
+ // create a string variable
+ String language = "Java";
+ // if statement
+ if (language == "Java") {
+ System.out.println("Best Programming Language");
+ }
+ }
+}
